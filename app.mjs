3caa933e6b28aa4b3abf141f36e6a916a5019bb5
adrianmarello <adrianmarello@gmail.com>
2024-12-03 10:24:22 -0300

    configs

--- app.mjs
+++ app.mjs
@@ -4,24 +4,20 @@ import superheroesRoutes from "./src/routes/superheroesRoutes.mjs";
 import path from 'path';
 import expressLayouts from 'express-ejs-layouts';
 import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+console.log(__dirname);
 app.set('view engine', 'ejs');
-
 app.set('views', path.join(__dirname, 'src','views'));
-
 app.use(expressLayouts);
-
 app.set('layout', path.join(__dirname, 'src', 'views', 'layout'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 connectDB();
