eedb29d2aa20099a4d1762bbb0cbf226ea01fa45
adrianmarello <adrianmarello@gmail.com>
2024-12-03 09:50:04 -0300

    configs

--- src/app.mjs
+++ src/app.mjs
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, './src/views'));
+app.set('views', path.join(__dirname, './views'));
 
 app.use(expressLayouts);
 
