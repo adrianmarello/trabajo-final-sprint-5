224ec756901cb171338f71f6e8c49f877a961297
adrian marello <adrianmarello@gmail.com>
2025-01-31 08:32:19 -0300

    listado de paises

--- src/controllers/paisesController.mjs
+++ src/controllers/paisesController.mjs
@@ -1,31 +1,64 @@
-import { crearPais } from "../services/paisesService.mjs";
-
+import { crearPais, obtenerTodosLosPaises } from "../services/paisesService.mjs";
 
 const COUNTRIES_API = process.env.COUNTRIES_API;
+const NAVBAR_LINKS = [
+    { text: 'Inicio', href: '/api/pais/ver-todos', icon: '/icons/home.svg'},
+    { text: 'Agregar Pais', href: '/api/pais/crear', icon: '/icons/home.svg'},
+  ]
 
-export function obtenerYGuardarPaises(req, res) {
+export async function obtenerYGuardarPaises(req, res) {
     const paises = [];
-    // obtener los paises y guardarlos en la base de datos
-    fetch(COUNTRIES_API)
-        .then(response => response.json())
-        .then(async (data) => {
+    try {
+        const response = await fetch(COUNTRIES_API);
+        const data = await response.json();
 
-            let testPais = data[0];
-            testPais.creador = 'Adrián Marello';
-            const newPais = await crearPais(testPais);
-            console.log(newPais);
+        // Filtramos los paises que hablan español
+        const paisesConEspanol = data.filter(pais => pais?.languages?.spa);
 
-            //data.forEach(async pais => {
-                //const newPais = await crearPais(pais);
-                //console.log(pais.capital);
-                //paises.push(newPais);
-            //});
-            console.log(paises)
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).json({ mensaje: 'Error al obtener los paises' });
+        // Creamos un array de promesas para insertar los paises
+        const promesas = paisesConEspanol.map(async pais => {
+            pais.creador = 'Adrián Marello'; // Agregamos el creador
+            await crearPais(pais); // Guardamos el pais en la base de datos
+            paises.push(pais); // Añadimos el pais al array para la respuesta
         });
 
-    res.status(200).json({ mensaje: 'Paises guardados exitosamente' });
+        // Esperamos a que todas las inserciones se completen
+        await Promise.all(promesas);
+
+        // Respondemos después de haber guardado todos los paises
+        res.status(200).json({ data: paises, mensaje: 'Paises guardados exitosamente' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Error al obtener los paises' });
+    }
+}
+
+export async function obtenerTodosLosPaisesController(req, res) {
+  try {
+    // Obtener todos los paises de la base de datos
+    const paises = await obtenerTodosLosPaises();
+    // Filtrar paises por creador y que contengan un nombre
+    const paisesFiltrados = paises.filter(pais => pais.creador === 'Adrián Marello' && pais.name.official);
+    // Población total
+    const poblacionTotal = paisesFiltrados.reduce((acc, pais) => acc + pais.population, 0);
+    // Área total
+    const areaTotal = paisesFiltrados.reduce((acc, pais) => acc + pais.area, 0);
+    // Gini promedio
+    const giniPromedio = paisesFiltrados.reduce(
+        (acc, pais) => acc + (pais?.gini[2019] ? pais?.gini[2019] : 0), 0
+    ) / paisesFiltrados.length;
+    
+    // Renderizar la vista 'dashboard' y pasarle los datos de los paises
+    res.render('dashboardPaises', { 
+      title: 'Dashboard de Paises',
+      navbarLinks: NAVBAR_LINKS,
+      paises: paisesFiltrados,
+      poblacion_total: poblacionTotal,
+      area_total: areaTotal,
+      gini_promedio: giniPromedio,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error al obtener los paises');
+  }
 }
--- src/routes/superheroesRoutes.mjs
+++ src/routes/superheroesRoutes.mjs
@@ -16,11 +16,12 @@ import {
   obtenerTodosLosSuperheroesController 
 } from '../controllers/superheroesController.mjs';
 import superheroValidations from '../validations/superheroValidation.mjs';
-import { obtenerYGuardarPaises } from '../controllers/paisesController.mjs';
+import { obtenerTodosLosPaisesController, obtenerYGuardarPaises } from '../controllers/paisesController.mjs';
 
 const router = express.Router();
 
 router.get('/obtener-y-guardar-paises', obtenerYGuardarPaises);
+router.get('/pais/ver-todos', obtenerTodosLosPaisesController);
 router.get('/heroes', obtenerTodosLosSuperheroesController);
 router.get('/heroes/crear', mostrarFormularioSuperheroeController);
 router.post('/heroes', superheroValidations, crearSuperHeroeController);
